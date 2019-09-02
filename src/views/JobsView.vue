c9ab86e2b73d8d4b81cf8668cdc0b1bd75b88b40
mooooburg <drawyourmind@naver.com>
2019-09-02 22:04:34 +0900

    190902_2204 get api / axios

--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,12 +1,27 @@
 <template>
   <div>
-      jobs
+      <div v-for="user in users">{{ user.title }}</div>
   </div>
 </template>
 
 <script>
+import { fetchJobsList } from '../api/index.js'
 export default {
+  data(){
+    return {
+      users: []
+    }
+  },
+  created(){
+    let vm = this;
+    fetchJobsList()
+      .then(function(response){
+        vm.users = response.data;
+      })
+      .catch(function(error){
 
+      })
+  }
 }
 </script>
 
